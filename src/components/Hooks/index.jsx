1b57f5b3a5dbf6bb54fc1e43545da1719d221db1
夏宁 <478084629@qq.com>
2022-02-20 23:47:56 +0800

    1

--- src/components/Hooks/index.jsx
+++ src/components/Hooks/index.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 
 export default function Hooks() {
-    const [a,b] = React.useState({name:'zhangsan'})
+    const [a,b] = React.useState([0,1,2])
     console.log(b);
     const add = (val) => {
-        b((state) => {
-            const b = {
-                a: val
-            }
-            return {...state,...b}
-        })
+        // b((state) => {
+            // const c = {
+            //     a: val
+            // }
+        //     return {...state,...b}
+        // })
+        b([...a,3])
     }
     console.log(a);
   return (
-    <div onClick={() => add('accc')}>{a.name}</div>
+    <div onClick={() => add('accc')}>{a[2]}</div>
   )
 }
