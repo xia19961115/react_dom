4036c4601b57501c2d29958162725b65344e8283
夏宁 <478084629@qq.com>
2022-06-16 00:24:48 +0800

    1

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,7 +2,9 @@ import React, { PureComponent } from 'react'
 import PubSub from 'pubsub-js';
 export default class List extends PureComponent {
     state ={
-        name:'ersan'
+        name:'ersan',
+        num: 1,
+        ar: []
     }
     Click() {
         console.log(this);
@@ -19,11 +21,42 @@ export default class List extends PureComponent {
         // console.log(obj === {...obj})
         this.setState({name:'aaa'})
     }
+    add = () => {
+        setTimeout(()=>{
+            // setState 使用函数方法 可接受(state,prop)
+            this.setState((state)=>{
+                console.log(state)
+            })
+        })
+        // this.setState((state)=>({num:state.num+1}))
+        // console.log(this.state.num);
+        // React 内部将同一事件响应函数中的多个setState进行合并，减少setState的调用次数，也就能减少渲染的次数，提高性能。
+        // setState调用是异步的 ,   再外层套个setTimeout 则为同步
+        // 在原生事件、setTimeout中使用setState，就不归 React 管理了，写了几次setState，就会调用几次setState。
+        this.setState({num:this.state.num+3})
+        console.log(this.state.num);
+        this.setState({num:this.state.num+1})
+    }
+    arr =() => {
+        // 复杂数据修改, 对象同理
+        /** 如
+         *  let obj = this.state.obj
+         *  obj.xxx = 'aa'
+         *  this.setState({obj})
+         */
+        let ar = this.state.ar
+        ar = [1,2,3]
+        this.setState({ar},()=>{
+            console.log(this.state.ar);
+        })
+    }
     render() {
         return (
             <div>
                 <button onClick={this.Click.bind(this)}>点击</button>
                 <div onClick={this.band}>{this.state.name}</div>
+                <button onClick={this.add}>加1</button>
+                <button onClick={this.arr}>数组</button>
             </div>
         )
     }
